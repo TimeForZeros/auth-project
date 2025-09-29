2b85682bcd1a4bf7e2a135a5f550860d5591f3e5
Victor Gonzalez <dazedbutamused@gmail.com>
2025-09-28 21:45:45 -0700

    fix some config

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,6 +4,6 @@ export const user = pgTable('user', {
   id: serial('id').primaryKey(),
   email: varchar('email', { length: 255 }).notNull().unique(),
   name: varchar('name', { length: 100 }),
-  password: varchar('password', {length: 97}),
+  password: varchar('password', { length: 97 }),
   createdAt: timestamp('created_at').defaultNow().notNull(),
 });
